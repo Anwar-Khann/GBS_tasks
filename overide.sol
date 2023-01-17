3269acb6f89020aa6850c0ca6027e3a39b8fdfb4
Anwar-Ul-Haq <106601317+Anwar-Khann@users.noreply.github.com>
2023-01-17 16:41:54 +0500

    Add files via upload

---
+++ overide.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("MyToken", "MTK") {
+        _mint(msg.sender, 1000000  * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public virtual onlyOwner {
+        _mint(to, amount);
+    }
+}
+
+
+contract Override is MyToken{
+
+        mapping(address => uint256) private _balances;
+
+       function mint(address to, uint256 amount) public virtual override(MyToken) onlyOwner {
+        _mint(to, amount);
+    }
+
+    // overiding approve
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        address owner = _msgSender();
+        _approve(owner, spender, amount);
+        return true;
+    }
+
+
+
+
+
+    //   OVERRIDING INCREASE ALLOWANCE
+
+    function increaseAllowance(address spender, uint256 addedValue) public virtual override returns (bool) {
+        address owner = _msgSender();
+        _approve(owner, spender, allowance(owner, spender) + addedValue);
+        return true;
+    }
+    //overriding decreaseAllowance
+
+     function decreaseAllowance(address spender, uint256 subtractedValue) public virtual override(ERC20) returns (bool) {
+        address owner = _msgSender();
+        uint256 currentAllowance = allowance(owner, spender);
+        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
+        unchecked {
+            _approve(owner, spender, currentAllowance - subtractedValue);
+        }
+
+        return true;
+    }
+
+
+    // overiding transfer
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override(ERC20) {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+
+        _beforeTokenTransfer(from, to, amount);
+
+        uint256 fromBalance = _balances[from];
+        require(fromBalance >= amount, "ERC20: transfer amount exceeds balance");
+        unchecked {
+            _balances[from] = fromBalance - amount;
+            // Overflow not possible: the sum of all balances is capped by totalSupply, and the sum is preserved by
+            // decrementing then incrementing.
+            _balances[to] += amount;
+        }
+
+        emit Transfer(from, to, amount);
+
+        _afterTokenTransfer(from, to, amount);
+    }
+
+    // overriding transferfrom
+
+     function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
+
+
+    //overriding transferownershhip
+
+    function transferOwnership(address newOwner) public virtual override onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+
+    // overridng renounceOwnership
+   function renounceOwnership() public virtual override onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+
+
+
+
+
+
+}
